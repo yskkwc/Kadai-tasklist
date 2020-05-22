d0cde69c47a41e968cf11c001c735b638cfe77f8
user <user@usernoair.lan>
2020-05-22 15:32:56 +0900

    Add new database

---
+++ src/models/Tasks.java
@@ -0,0 +1,73 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+            )
+})
+@Table(name = "tasks")
+
+public class Tasks {
+    //テーブルの要素(id/created_date/updated_date/contents)
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_date", nullable = false)
+    private Timestamp created_date;
+
+    @Column(name = "updated_date", nullable = false)
+    private Timestamp updated_date;
+
+    @Column(name = "content", length = 300, nullable = false)
+    private String content;
+
+    //getset
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(Timestamp created_date) {
+        this.created_date = created_date;
+    }
+
+    public Timestamp getUpdated_date() {
+        return updated_date;
+    }
+
+    public void setUpdated_date(Timestamp updated_date) {
+        this.updated_date = updated_date;
+    }
+
+    public String getContents() {
+        return content;
+    }
+
+    public void setContents(String content) {
+        this.content = content;
+    }
+
+
+}
